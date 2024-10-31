a7eec5ee84a649e1bd1a5e2ed33ea080fc501717
lukyamuzi gabriella <g.bayinze004@gmail.com>
2024-10-31 17:27:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 1278;
+const port = 1256;
 
 //GET
 app.get('/greetings',(request, response) =>{
@@ -9,13 +9,13 @@ app.get('/greetings',(request, response) =>{
 
 ///list of friends
 
-let friends = [{"id": 1,"name":"Mama"},
+let friends1 = [{"id": 1,"name":"Mama"},
     {"id":1,"name":"Danny"},
-    {"id":1,"name":"Hassan"},
-    {"id":1,"name":"Husein"},
-    {"id":1,"name":"Daddy"}];
-app.get('/list-of-friends', (request,response) =>{
-    return response.send(friends)
+    {"id":2,"name":"Hassan"},
+    {"id":3,"name":"Hussein"},
+    {"id":4,"name":"Daddy"}];
+app.get('/list-of-friends1', (request,response) =>{
+    return response.send(friends1)
 
 });
 
